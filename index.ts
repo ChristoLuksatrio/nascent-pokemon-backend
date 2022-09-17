933febc3b32e5e53d102c39be47a4a117d6caf7e
ChristoLuksatrio <christo.luksatrio@gmail.com>
2022-09-17 10:57:38 -0400

    added mongodb connection

--- index.ts
+++ index.ts
@@ -3,9 +3,14 @@ import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import resolvers from "./resolvers";
 import typeDefs from "./typeDefs";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
 const initServer = async () => {
   const app = express();
+  app.use(cors());
+  dotenv.config();
 
   const apolloServer = new ApolloServer({
     typeDefs,
@@ -15,10 +20,17 @@ const initServer = async () => {
   apolloServer.applyMiddleware({ app });
 
   app.use((req, res) => {
-    res.send("Server started");
+    res.send("Behold! My pokemon form server has started");
   });
 
   const PORT = process.env.PORT || 5000;
+  const connectionString = process.env.mongodb || "";
+  try {
+    await mongoose.connect(connectionString);
+    console.log(`Mongoose connected on port ${PORT}`);
+  } catch (error) {
+    console.log(error);
+  }
 
   app.listen(PORT, () =>
     console.log(`Express server is running on port http://localhost:${PORT}`)
